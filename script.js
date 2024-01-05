834fb9820c302e3c77e0d72925ee929dd64163d6
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2024-01-05 20:03:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //complete this code
 class Animal {
-	constructor(species) {
+  constructor(species) {
     this._species = species;
   }
 
@@ -9,20 +9,19 @@ class Animal {
   }
 
   makeSound() {
-    console.log("Some generic animal sound");
+    console.log(`The ${this.species} makes a sound`);
   }
 }
 
-class Dog extends Animal {
-	purr() {
-    console.log("purr");
-	}
-}
-
 class Cat extends Animal {
-	bark() {
+  makeSound() {
+    console.log("purr");
+  }
+}
+class Dog extends Animal {
+  makeSound() {
     console.log("woof");
-	}
+  }
 }
 // Do not change the code below this line
 window.Animal = Animal;
